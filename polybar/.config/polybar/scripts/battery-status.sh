ea4350ccc2d41f205958029e0004bf93ca2658d9
ncapel <capel.nicholas@gmail.com>
2025-07-04 10:59:23 -0400

    bugfix: fixed volume display

--- polybar/.config/polybar/scripts/battery-status.sh
+++ polybar/.config/polybar/scripts/battery-status.sh
@@ -5,18 +5,7 @@ battery=$(acpi -b)
 percent=$(echo "$battery" | grep -o '[0-9]\+%' | tr -d '%')
 status=$(echo "$battery" | awk '{print $3}' | tr -d ',')
 
-if [[ "$status" == "Charging" ]]; then
-    icon="⚡"
-elif (( percent >= 90 )); then
-    icon=""
-elif (( percent >= 70 )); then
-    icon=""
-elif (( percent >= 50 )); then
-    icon=""
-elif (( percent >= 30 )); then
-    icon=""
-else
-    icon=""
-fi
 
-echo "$icon $percent%"
+# echo "$icon $percent%"
+
+echo "$percent%"
--- polybar/.config/polybar/scripts/volume.sh
+++ polybar/.config/polybar/scripts/volume.sh
@@ -2,15 +2,13 @@
 
 WPCTL_OUTPUT=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
 
-if [[ $WPCTL_OUTPUT == *"MUTED"* ]]; then
+if echo "$WPCTL_OUTPUT" | grep -q "Mute: true"; then
   echo "muted"
   exit 0
 fi
 
-VOLUME=$(echo "$WPCTL_OUTPUT" | sed -n 's/^Volume: \([0-9.]*\).*/\1/p')
+VOLUME_FLOAT=$(echo "$WPCTL_OUTPUT" | sed -n 's/^Volume: \([0-9.]*\).*/\1/p')
 
-if [[ $VOLUME == *"."* ]]; then
-  VOLUME=$(echo "scale=0; $VOLUME / 1" | bc)
-fi
+VOLUME_PERCENT=$(printf "%.0f" "$(echo "$VOLUME_FLOAT * 100" | bc -l)")
 
-echo "${VOLUME}%"
+echo "${VOLUME_PERCENT}%"
